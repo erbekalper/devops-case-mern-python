dcc6e6cdd2d37e07cc02188d67b629ab415d4666
Alper Tolga ERBEK <alpertolgaerbek@gmail.com>
2026-06-30 23:39:25 +0300

    Fix frontend API endpoint for cloud deployment

--- mern-project/client/src/components/healthcheck.js
+++ mern-project/client/src/components/healthcheck.js
@@ -4,7 +4,7 @@ export default function HealthStatus() {
   const [status, setStatus] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5050/healthcheck/")
+    fetch("http://51.20.131.236:30505/healthcheck/")
       .then((response) => response.json())
       .then((data) => setStatus(data));
   }, []);
